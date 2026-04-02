bab3895aeb2598de0c216e61266beb6520fb48d1
RadiatingVibe <dhruvselopalrocks@gmail.com>
2026-04-02 12:55:21 +0530

    add env to git ignore

--- wysa-project-backend/src/server.ts
+++ wysa-project-backend/src/server.ts
@@ -33,7 +33,7 @@ import { QARepoistory } from "./repoistory/qa-repoistory";
 
 
   // ================= SERVER =================
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   server.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
